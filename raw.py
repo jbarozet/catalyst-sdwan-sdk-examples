03beae2de0a470c35dcfb56d988d844349a1f129
Jean-Marc Barozet <jbarozet@gmail.com>
2023-11-02 13:43:36 +0100

    load vmanage details from env parameters

--- raw.py
+++ raw.py
@@ -9,7 +9,10 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # CREATE SESSION
 
-
+url = os.environ.get("vmanage_host")
+# vmanage_port = os.environ.get("vmanage_port")
+username = os.environ.get("vmanage_username")
+password = os.environ.get("vmanage_password")
 
 session = create_vManageSession(url=url, username=username, password=password)
 print(session.about())
