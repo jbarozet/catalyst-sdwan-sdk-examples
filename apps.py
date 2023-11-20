c6054f81ed28e66d0da986970e2026642fcf1925
Jean-Marc Barozet <jbarozet@gmail.com>
2023-11-20 10:38:22 +0100

    updates

--- apps.py
+++ apps.py
@@ -1,9 +1,11 @@
-from vmngclient.dataclasses import Personality
-from vmngclient.session import create_vManageSession
-import urllib3
+import os
+
 import click
 import tabulate
-import os
+import urllib3
+
+# from vmngclient.dataclasses import Personality
+from vmngclient.session import create_vManageSession
 
 # Disable warnings because of no certificate on vManage
 # urllib3.disable_warnings()
@@ -13,28 +15,58 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 # Click CLI
 # ----------------------------------------------------------------------------------------------------
 
+
 @click.group()
 def cli():
-    """Command line tool to showcase Catalyst SD-WAN Python SDK
-    """
+    """Command line tool to showcase Catalyst SD-WAN Python SDK"""
     pass
+
 
 # ----------------------------------------------------------------------------------------------------
 # app-list
 # ----------------------------------------------------------------------------------------------------
 
+
 @click.command()
 def app_list():
-    # Raw APIs
+    """Retrieve the list of Applications.
+    Example command: ./apps.py app-list
+    """
+    # Using raw APIs
     response = session.get("/dataservice/device/dpi/application-mapping")
+    payload = response.json()
+
+    # Format output
+    table = list()
+    app_headers = ["App name", "Family", "ID"]
+
+    for item in payload["data"]:
+        tr = [item["name"], item["family"], item["appId"]]
+        table.append(tr)
+
+    click.echo(tabulate.tabulate(table, app_headers, tablefmt="fancy_grid"))
+
+
+# ----------------------------------------------------------------------------------------------------
+# approute-fields
+# ----------------------------------------------------------------------------------------------------
+
+
+@click.command()
+def approute_fields():
+    """Retrieve App route Aggregation API Query fields.
+    Example command: ./apps.py approute-fields
+    """
+    # Using raw APIs
+    response = session.get("/dataservice/statistics/approute/fields")
     payload = response.json()
 
     ## Format output
     table = list()
-    app_headers = ["App name", "Family", "ID"]
+    app_headers = ["Property", "Type"]
 
-    for item in payload['data']:
-        tr = [item['name'], item['family'], item['appId']]
+    for item in payload:
+        tr = [item["property"], item["dataType"]]
         table.append(tr)
 
     click.echo(tabulate.tabulate(table, app_headers, tablefmt="fancy_grid"))
@@ -50,12 +82,16 @@ username = os.environ.get("vmanage_username")
 password = os.environ.get("vmanage_password")
 
 if url is None or username is None or password is None:
-    print("For Windows Workstation, vManage details must be set via environment variables using below commands")
+    print(
+        "For Windows Workstation, vManage details must be set via environment variables using below commands"
+    )
     print("set vmanage_host=10.10.1.1")
     print("set vmanage_port=8443")
     print("set vmanage_username=admin")
     print("set vmanage_password=admin")
-    print("For MAC OSX Workstation, vManage details must be set via environment variables using below commands")
+    print(
+        "For MAC OSX Workstation, vManage details must be set via environment variables using below commands"
+    )
     print("export vmanage_host=10.10.1.1")
     print("export vmanage_port=8443")
     print("export vmanage_username=admin")
@@ -70,6 +106,7 @@ session = create_vManageSession(url=url, username=username, password=password)
 # ----------------------------------------------------------------------------------------------------
 
 cli.add_command(app_list)
+cli.add_command(approute_fields)
 
 if __name__ == "__main__":
     cli()
--- devices.py
+++ devices.py
@@ -1,24 +1,46 @@
-import json
+import os
+
+import urllib3
 from vmngclient.dataclasses import Personality
 from vmngclient.session import create_vManageSession
-import urllib3
-import os
 
 # Disable warnings because of no certificate on vManage
 # urllib3.disable_warnings()
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# CREATE SESSION
 
+# ----------------------------------------------------------------------------------------------------
+# Create session
+# ----------------------------------------------------------------------------------------------------
+#
 url = os.environ.get("vmanage_host")
 # vmanage_port = os.environ.get("vmanage_port")
 username = os.environ.get("vmanage_username")
 password = os.environ.get("vmanage_password")
 
+if url is None or username is None or password is None:
+    print(
+        "For Windows Workstation, vManage details must be set via environment variables using below commands"
+    )
+    print("set vmanage_host=10.10.1.1")
+    print("set vmanage_port=8443")
+    print("set vmanage_username=admin")
+    print("set vmanage_password=admin")
+    print(
+        "For MAC OSX Workstation, vManage details must be set via environment variables using below commands"
+    )
+    print("export vmanage_host=10.10.1.1")
+    print("export vmanage_port=8443")
+    print("export vmanage_username=admin")
+    print("export vmanage_password=admin")
+    exit()
+
 session = create_vManageSession(url=url, username=username, password=password)
 print(session.about())
 
-# GET THE LIST OD DEVICES
+# ----------------------------------------------------------------------------------------------------
+# Get the list of devices
+# ----------------------------------------------------------------------------------------------------
 
 devices = session.api.devices.get()
 
@@ -26,11 +48,17 @@ devices = session.api.devices.get()
 vmanage = devices.filter(personality=Personality.VMANAGE).single_or_default()
 
 print("---------------------------------------------------------------------")
-print(f"Hostname: {vmanage.hostname} - Load: {vmanage.cpu_load} - Board serial: {vmanage.board_serial}")
+print(
+    f"Hostname: {vmanage.hostname} - Load: {vmanage.cpu_load} - Board serial: {vmanage.board_serial}"
+)
 print("---------------------------------------------------------------------")
 
-# List all devices in the overlay
+# ----------------------------------------------------------------------------------------------------
+# Display the list of devices
+# ----------------------------------------------------------------------------------------------------
 
 for dev in devices:
     print(f"{dev.hostname} - Load: {dev.cpu_load} - Board serial: {dev.board_serial}")
     print(session.api.device_state.get_system_status(dev.id))
+
+# ---END--
