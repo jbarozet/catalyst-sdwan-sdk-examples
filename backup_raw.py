1f508b93dd82fe3294cf7d90a30e49e2bf69b0b0
Jean-Marc Barozet <jbarozet@gmail.com>
2024-03-19 15:39:37 +0100

    save config-groups and profiles in files

---
+++ backup_raw.py
@@ -0,0 +1,123 @@
+# Backup Task
+#
+# config_groups
+#   associated
+#   groups
+#   values
+#
+# feature_profiles
+#   sdwan
+#       cli
+#       system
+#       transport
+#       service
+#       policy_object
+
+import json
+import os
+from os.path import join
+from pathlib import Path
+
+import urllib3
+from catalystwan.api.config_group_api import ConfigGroupAPI, ConfigGroupResponsePayload
+
+from session import create_session
+
+profile_id_table = []
+
+
+def create_backup_dir():
+    workdir = "data/groups"
+    # os.mkdir(workdir)
+    # Check if workdir folder already exists. If yes, then stop backup process
+    workdir = join(os.path.abspath(os.getcwd()), workdir)
+    if os.path.isdir(workdir):
+        exit(
+            f"{workdir} folder is already in use. Please select a different workdir directory."
+        )
+
+
+def save_config_groups():
+    """
+    List specific config-group details
+    Get the list of feature profiles used but not parcels
+    Need to use the API for profiles to get the list of associated parcels
+    """
+
+    workdir = "data/config_groups"
+    base = "dataservice/v1/config-group/"
+    data = session.get(base).json()
+
+    for key in data:
+        config_group_id = key["id"]
+        config_group_name = key["name"]
+        tmp = config_group_name + ".json"
+        filename = join(workdir, tmp)
+        print(f"> Config Group ID ❯ {config_group_name}")
+        url = base + config_group_id
+        config_group = session.get(url).json()
+        for item in config_group["profiles"]:
+            profile_id = item["id"]
+            print(profile_id)
+            profile_id_table.append(profile_id)
+        with open(filename, "w") as file:
+            json.dump(config_group, file, indent=4)
+    print(f"summary: {profile_id_table}")
+
+
+def save_feature_profiles():
+    """
+    Feature Profiles - Get specific profile details
+    Including associated parcels
+    """
+
+    base = "dataservice/v1/feature-profile/sdwan/service/"
+    workdir = "data/feature_profiles"
+    # feature_profile_id = input("Enter feature-profile ID ❯ ")
+    for profile_id in profile_id_table[:]:
+        url = base + profile_id
+        data = session.get(url).json()
+        profile_name = data["profileName"]
+        print(f"--- Profile Name ❯ {profile_name}")
+        tmp = profile_name + ".json"
+        filename = join(workdir, tmp)
+        with open(filename, "w") as file:
+            json.dump(data, file, indent=4)
+
+
+def save_associated_devices():
+    """
+    Get Devices associated with a specific config-group.
+    'v1/config-group/{configGroupId}/device/associate'
+    """
+
+
+def config_group_values():
+    """
+    'v1/config-group/{configGroupId}/device/variables'
+    store_path = ('config_groups', 'values')
+    store_file = '{item_name}.json'
+    """
+
+
+def save_automated_rules():
+    # base = "dataservice/tag/tagRules/"
+    # base = "v1/config-group/{configGroupId}/rules"
+    base = "dataservice/v1/config-group/"
+    config_group_id = input("Enter config-group id: ")
+    url = base + config_group_id + "/rules"
+    data = session.get(url).json()
+    data_formatted = json.dumps(data, indent=4)
+    print(data_formatted)
+
+
+# Disable warnings because of no certificate on vManage
+# urllib3.disable_warnings()
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Create vManage session
+session = create_session()
+
+save_config_groups()
+save_feature_profiles()
+# save_automated_rules()
